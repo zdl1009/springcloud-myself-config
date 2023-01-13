bea1a042e4ee68dcf7b73df47da02bc01a092a6e
zdl <1045510925@qq.com>
2023-01-13 13:56:24 +0800

    fix:update a new project

---
+++ src/main/java/com/eurke/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.eurke;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+        log.info("启动成功");
+    }
+}
